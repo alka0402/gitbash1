938fba6be73a33dd6d0c66230d9ca4ea61632cb7
alka0402 <asatialka0@gmail.com>
2025-07-15 15:55:10 +0530

    push

---
+++ src/gitbash_practise/sample.java
@@ -0,0 +1,10 @@
+package gitbash_practise;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module gitbash_practise {
+}
