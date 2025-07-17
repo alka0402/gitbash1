49ae4864da95da4f4fef606a42c3f0ddf3acb67d
alka0402 <asatialka0@gmail.com>
2025-07-17 15:01:30 +0530

    pusingggggggg

--- src/gitbash_practise/sample.java
+++ src/gitbash_practise/sample.java
@@ -4,12 +4,12 @@ public class sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("hellow");
-		//lkop
-=======
+		
+
 		//pull
->>>>>>> c4dd7a05c1614c53dd3d52528c2be87af025849e
+
 
 	}
 
